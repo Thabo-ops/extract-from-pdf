68ee457ee979a9cb9ee06d679653b93097e9af0e
Thabo <thabo@basalt.co>
2019-11-13 14:25:26 +0200

    Before migration

--- index.js
+++ index.js
@@ -1,7 +1,34 @@
 const PDFExtract = require('pdf.js-extract').PDFExtract;
 const pdfExtract = new PDFExtract();
-const options = {}; /* see below */
-pdfExtract.extract('Ngubane_Thabo.pdf', options, (err, data) => {
-  if (err) return console.log(err);
-  console.log(data.pages[0]);
-});
+// const options = {}; /* see below */
+// pdfExtract.extract('Ngubane_Thabo.pdf', options, (err, data) => {
+//   if (err) return console.log(err);
+//   console.log(data.pages[0]);
+// });
+
+
+// const WordExtractor = require("word-extractor");
+// const extractor = new WordExtractor();
+// const extracted = extractor.extract("word-template.doc");
+// extracted.then((doc) => {
+// console.log(doc);
+// });
+
+
+// const extracted = extractor.extract("Style ID Roadmap.docx");
+// extracted.then((doc) => {
+// console.log(doc);
+// });
+
+
+var toPdf = require("office-to-pdf")
+var fs = require("fs")
+var wordBuffer = fs.readFileSync("./Style ID Roadmap.docx")
+
+toPdf(wordBuffer).then(
+  (pdfBuffer) => {
+    fs.writeFileSync("./test.pdf", pdfBuffer)
+  }, (err) => {
+    console.log(err)
+  }
+)
